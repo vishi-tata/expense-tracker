92757d743c0224ac671a766c682c3bc9c4402ff2
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-09-07 23:13:20 +0530

    Fitlering expenses by year markup added

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFitler from "./ExpensesFilter";
@@ -5,32 +7,38 @@ import ExpensesFitler from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <div>
-      <ExpensesFitler />
-      <Card className="expenses">
-        <ExpenseItem
-          date={props.expenses[0].date}
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-        />
-        <ExpenseItem
-          date={props.expenses[1].date}
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-        />
-        <ExpenseItem
-          date={props.expenses[2].date}
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-        />
-        <ExpenseItem
-          date={props.expenses[3].date}
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-        />
-      </Card>
-    </div>
+    <Card className="expenses">
+      <ExpensesFitler
+        onChangeFilter={filterChangeHandler}
+        filteredYear={filteredYear}
+      />
+      <ExpenseItem
+        date={props.expenses[0].date}
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+      />
+      <ExpenseItem
+        date={props.expenses[1].date}
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+      />
+      <ExpenseItem
+        date={props.expenses[2].date}
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+      />
+      <ExpenseItem
+        date={props.expenses[3].date}
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+      />
+    </Card>
   );
 };
 
--- src/components/Expenses/ExpensesFilter.jsx
+++ src/components/Expenses/ExpensesFilter.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-
 import "./ExpensesFilter.css";
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select onChange={dropdownChangeHandler} value={props.filteredYear}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
