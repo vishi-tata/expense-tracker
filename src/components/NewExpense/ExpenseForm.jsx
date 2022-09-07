d52ef5c6b2acff30d723e3bdefee6a85b85361b9
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-09-07 19:57:11 +0530

    onChange handlers added to input elements

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,20 +1,38 @@
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const titleChangeHandler = (event) => {};
+
+  const amountChangeHandler = (event) => {};
+
+  const dateChangeHandler = (event) => {};
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" />
+          <input type="text" id="title" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" id="amount" min="0.01" step="0.01" />
+          <input
+            type="number"
+            id="amount"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            id="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
