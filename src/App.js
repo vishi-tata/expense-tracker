a17ac8c57c62ac8b2384e39e24d4e770f165da72
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-09-07 19:09:31 +0530

    Use state feature to change expense title added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
