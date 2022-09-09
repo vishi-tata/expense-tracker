bf818917f8966508e1987003c30d115c5ab5fcc2
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-09-09 11:22:19 +0530

    Modified NewExpense.jsx (Editing to Adding)

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -4,31 +4,31 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-  const [isEditing, setIsEditing] = useState(false);
+  const [isAdding, setIsAdding] = useState(false);
 
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = { ...enteredExpenseData, id: Math.random().toString() };
     props.onAddExpense(expenseData);
-    setIsEditing(false);
+    setIsAdding(false);
   };
 
-  const startEditingHandler = () => {
-    setIsEditing(true);
+  const startAddingHandler = () => {
+    setIsAdding(true);
   };
 
-  const stopEditingHandler = () => {
-    setIsEditing(false);
+  const stopAddingHandler = () => {
+    setIsAdding(false);
   };
 
   return (
     <div className="new-expense">
-      {!isEditing && (
-        <button onClick={startEditingHandler}>Add New Expense</button>
+      {!isAdding && (
+        <button onClick={startAddingHandler}>Add New Expense</button>
       )}
-      {isEditing && (
+      {isAdding && (
         <ExpenseForm
           onSaveExpenseData={saveExpenseDataHandler}
-          onCancel={stopEditingHandler}
+          onCancel={stopAddingHandler}
         />
       )}
     </div>
